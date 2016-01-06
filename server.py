84c5b1e2741cb75a50a3295559436aa4fbef13a5
Robin Winslow <robin@robinwinslow.co.uk>
2016-01-06 13:24:03 +0000

    Make into generic episode finder & provide WSGI server

---
+++ server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import subprocess
+import os
+import re
+from werkzeug.wrappers import Request, Response
+
+def application(environ, start_response):
+    request = Request(environ)
+    query = request.args.get('q', '')
+
+    rss_response = subprocess.check_output(
+        ['./episode-finder.py', query]
+    )
+
+    response = Response(rss_response, mimetype='application/rss+xml')
+    return response(environ, start_response)
+
+if __name__ == '__main__':
+    from werkzeug.serving import run_simple
+    run_simple('127.0.0.1', 8111, application, use_debugger=True, use_reloader=True)
+
