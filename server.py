b558ab6666d0263cf4a40c7aa9df7f464a888977
Robin Winslow <robin@robinwinslow.co.uk>
2016-01-06 18:19:23 -0500

    Use generically accessible server

--- server.py
+++ server.py
@@ -18,5 +18,5 @@ def application(environ, start_response):
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
-    run_simple('127.0.0.1', 8111, application, use_debugger=True, use_reloader=True)
+    run_simple('0.0.0.0', 8111, application, use_debugger=True, use_reloader=True)
 
